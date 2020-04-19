9eaff42f7b2ae472887bbcee35605920097de484
Vincent Touchard <touchardv@gmail.com>
2020-04-19 17:42:15 +0200

    Rework BLE tracker.
    Don't scan all the time, use a timer with some randomness durations.

--- device/bletracker.go
+++ device/bletracker.go
@@ -1,6 +1,9 @@
 package device
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/bettercap/gatt"
 	log "github.com/sirupsen/logrus"
 	"github.com/touchardv/myhome-presence/config"
@@ -38,9 +41,20 @@ func (t *bleTracker) init(devices []config.Device, f func(p gatt.Peripheral, a *
 func (t *bleTracker) onDeviceStateChanged(d gatt.Device, s gatt.State) {
 	log.Debug("BLE device state:", s)
 	if s == gatt.StatePoweredOn {
-		d.Scan([]gatt.UUID{}, false)
-		t.scanning = true
+		t.startScanning()
 	}
+}
+
+func (t *bleTracker) startScanning() {
+	log.Debug("Start BLE device scanning...")
+	t.device.Scan([]gatt.UUID{}, false)
+	t.scanning = true
+}
+
+func (t *bleTracker) stopScanning() {
+	log.Debug("Stop BLE device scanning...")
+	t.device.StopScanning()
+	t.scanning = false
 }
 
 func (t *bleTracker) Track(devices []config.Device, presence chan string, stopping chan struct{}) {
@@ -55,14 +69,38 @@ func (t *bleTracker) Track(devices []config.Device, presence chan string, stoppi
 	if err != nil {
 		log.Error("Failed to init BLE device: ", err)
 	}
+	timer := time.NewTimer(30 * time.Second)
 	for {
 		select {
-		case <-stopping:
+		case <-timer.C:
 			if t.scanning {
-				t.device.StopScanning()
+				t.stopScanning()
+			} else {
+				t.startScanning()
+			}
+			timer.Reset(randomDuration(t.scanning))
+		case <-stopping:
+			timer.Stop()
+			if t.scanning {
+				t.stopScanning()
 			}
 			log.Info("Stopped: ble tracker")
 			return
 		}
 	}
+}
+
+const minScanDuration = 20
+const minDurationBetweenScans = 15
+
+func randomDuration(scanning bool) time.Duration {
+	var n int
+	if scanning {
+		n = minScanDuration
+		n += rand.Intn(10)
+	} else {
+		n = minDurationBetweenScans
+		n += rand.Intn(30)
+	}
+	return time.Duration(n) * time.Second
 }

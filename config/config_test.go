3016a51da453f0d797f7aaaae2fafd19e8abc55f
Vincent Touchard <touchardv@gmail.com>
2020-04-30 15:52:03 +0200

    Add extra configuration field to device: "bt_address"
    A non BLE device needs to be differentiated from "normal" BT devices (e.g. an Android mobile phone). These do not necessarily advertise but it is possible to "ping" these (e.g. using the Linux BlueZ tool "l2ping".

--- config/config_test.go
+++ config/config_test.go
@@ -15,4 +15,12 @@ func TestRetrieving(t *testing.T) {
 	device := cfg.Devices[0]
 	assert.Equal(t, "My laptop", device.Description)
 	assert.Equal(t, 2, len(device.IPInterfaces))
+
+	device = cfg.Devices[1]
+	assert.Equal(t, "My smartwatch", device.Description)
+	assert.Equal(t, "9d329f8ba3c24ae0a494b195dda27d41", device.BLEAddress)
+
+	device = cfg.Devices[2]
+	assert.Equal(t, "My phone", device.Description)
+	assert.Equal(t, "AA:BB:CC:DD:EE", device.BTAddress)
 }

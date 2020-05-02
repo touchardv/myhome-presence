b0be76522c8bb4ce53f2ebfb5b38abe26b3402be
Vincent Touchard <touchardv@gmail.com>
2020-05-02 16:00:17 +0200

    Release the BLE device when not scanning.
    Note: this requires gatt Device.Stop() to work properly.

--- device/bletracker.go
+++ device/bletracker.go
@@ -13,62 +13,76 @@ type bleTracker struct {
 	device   gatt.Device
 	devices  map[string]config.Device
 	scanning bool
+	presence chan string
 }
 
 func newBLETracker() Tracker {
-	d, err := gatt.NewDevice(defaultClientOptions...)
-	if err != nil {
-		log.Fatal("Failed to create BLE device: ", err)
-	}
 	return &bleTracker{
-		device:   d,
 		devices:  make(map[string]config.Device, 10),
 		scanning: false,
 	}
 }
 
-func (t *bleTracker) init(devices []config.Device, f func(p gatt.Peripheral, a *gatt.Advertisement, rssi int)) error {
+func (t *bleTracker) init(devices []config.Device, presence chan string) {
 	for _, device := range devices {
 		if len(device.BLEAddress) == 0 {
 			continue
 		}
 		t.devices[device.BLEAddress] = device
 	}
-	t.device.Handle(gatt.PeripheralDiscovered(f))
-	return t.device.Init(t.onDeviceStateChanged)
+	t.presence = presence
 }
 
 func (t *bleTracker) onDeviceStateChanged(d gatt.Device, s gatt.State) {
-	log.Debug("BLE device state:", s)
+	log.Debug("BLE device state: ", s)
 	if s == gatt.StatePoweredOn {
-		t.startScanning()
+		if !t.scanning {
+			d.Scan([]gatt.UUID{}, true)
+			t.scanning = true
+		}
+	}
+}
+
+func (t *bleTracker) onPeripheralDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
+	log.Debugf("Discovered %s %s %s", p.ID(), p.Name(), a.LocalName)
+	d, ok := t.devices[p.ID()]
+	if ok {
+		t.presence <- d.Identifier
 	}
 }
 
 func (t *bleTracker) startScanning() {
 	log.Debug("Start BLE device scanning...")
-	t.device.Scan([]gatt.UUID{}, false)
-	t.scanning = true
+	d, err := gatt.NewDevice(defaultClientOptions...)
+	if err != nil {
+		log.Fatal("Failed to create BLE device: ", err)
+		return
+	}
+	if err := d.Init(t.onDeviceStateChanged); err != nil {
+		log.Error("Failed to init BLE device: ", err)
+		return
+	}
+	t.device = d
+	t.device.Handle(gatt.PeripheralDiscovered(t.onPeripheralDiscovered))
 }
 
 func (t *bleTracker) stopScanning() {
 	log.Debug("Stop BLE device scanning...")
-	t.device.StopScanning()
-	t.scanning = false
+	if t.scanning == true {
+		t.device.StopScanning()
+		t.scanning = false
+	}
+	err := t.device.Stop()
+	if err != nil {
+		log.Error("Failed to stop device: ", err)
+	}
+	t.device = nil
 }
 
 func (t *bleTracker) Track(devices []config.Device, presence chan string, stopping chan struct{}) {
 	log.Info("Starting: ble tracker")
-	err := t.init(devices, func(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
-		log.Debugf("Discovered %s %s %s", p.ID(), p.Name(), a.LocalName)
-		d, ok := t.devices[p.ID()]
-		if ok {
-			presence <- d.Identifier
-		}
-	})
-	if err != nil {
-		log.Error("Failed to init BLE device: ", err)
-	}
+	t.init(devices, presence)
+	t.startScanning()
 	timer := time.NewTimer(30 * time.Second)
 	for {
 		select {

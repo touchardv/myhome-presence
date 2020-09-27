39c844ec38732db36a5b1297527f3a49a47333aa
Vincent Touchard <touchardv@gmail.com>
2020-09-27 12:22:52 +0200

    Add model for abstracting the device interfaces

---
+++ model/interfaces_test.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"encoding/json"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJSONSerialization(t *testing.T) {
+	i := InterfaceIPv4
+	data, _ := json.Marshal(i)
+	assert.Equal(t, `"ipv4"`, string(data))
+}
+
+func TestJSONDeSerialization(t *testing.T) {
+	data := `"ethernet"`
+	var i InterfaceType
+	err := json.Unmarshal([]byte(data), &i)
+	assert.Nil(t, err)
+	assert.Equal(t, InterfaceEthernet, i)
+
+	data = `"foobar"`
+	err = json.Unmarshal([]byte(data), &i)
+	assert.Nil(t, err)
+	assert.Equal(t, InterfaceUnknown, i)
+}
+
+func TestYAMLSerialization(t *testing.T) {
+	i := InterfaceBluetoothLowEnergy
+	data, _ := yaml.Marshal(i)
+	assert.Equal(t, "ble\n", string(data))
+}
+
+func TestYAMLDeserialization(t *testing.T) {
+	data := "bluetooth"
+	var i InterfaceType
+	err := yaml.Unmarshal([]byte(data), &i)
+	assert.Nil(t, err)
+	assert.Equal(t, InterfaceBluetooth, i)
+
+	data = "foobar"
+	err = yaml.Unmarshal([]byte(data), &i)
+	assert.Nil(t, err)
+	assert.Equal(t, InterfaceUnknown, i)
+}

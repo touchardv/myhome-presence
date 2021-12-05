7b27de67c56f4bd07641ac94f3ac23158737889d
Vincent Touchard <touchardv@gmail.com>
2021-12-05 15:01:16 +0100

    Implement tracker using TP-Link web interface.

--- trackers/tplink/trackers.go
+++ trackers/tplink/trackers.go
@@ -3,7 +3,9 @@ package tplink
 import (
 	"context"
 	"sync"
+	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/touchardv/myhome-presence/device"
 	"github.com/touchardv/myhome-presence/model"
 )
@@ -14,25 +16,81 @@ func EnableTrackers() {
 	device.Register("tplink-re450", newRE450Tracker)
 }
 
-func newArcherC2600Tracker() device.Tracker {
-	return &tplinkTracker{}
-}
+type loginFunc func(baseUrl string, username string, password string) (credentials, error)
 
-func newRE450Tracker() device.Tracker {
-	return &tplinkTracker{}
-}
+type statusFunc func(baseUrl string, c credentials) (statusResponse, error)
 
 type tplinkTracker struct {
+	name     string
+	baseURL  string
+	username string
+	password string
+	login    loginFunc
+	status   statusFunc
+}
+
+type credentials struct {
+	Token string
+	Nonce string
+}
+
+type loginResponse struct {
+	Data      map[string]string `json:"data"`
+	ErrorCode string            `json:"errorcode"`
+	Success   bool              `json:"success"`
+}
+
+type statusResponse struct {
+	Success   bool       `json:"success"`
+	ErrorCode string     `json:"errorcode"`
+	Data      statusData `json:"data"`
+}
+
+type statusDataDevice struct {
+	MACAddress string `json:"macaddr"`
+	IPAddress  string `json:"ipaddr"`
+	Hostname   string `json:"hostname"`
 }
 
 func (t *tplinkTracker) Loop(deviceReport device.ReportPresenceFunc, ctx context.Context, wg *sync.WaitGroup) error {
 	defer wg.Done()
 
-	<-ctx.Done()
+	log.Infof("Starting: %s tracker", t.name)
+	ticker := time.NewTicker(1 * time.Second)
 
-	return nil
+	for {
+		select {
+		case <-ctx.Done():
+			ticker.Stop()
+			log.Infof("Stopped: %s tracker", t.name)
+			return nil
+
+		case <-ticker.C:
+			ticker.Reset(5 * time.Minute)
+			c, err := t.login(t.baseURL, t.username, t.password)
+			if err != nil {
+				log.Errorf("[%s] login failed: %s", t.name, err)
+				break
+			}
+			r, err := t.status(t.baseURL, c)
+			if err != nil {
+				log.Errorf("[%s] status failed: %s", t.name, err)
+				break
+			}
+			if !r.Success {
+				log.Error(r.ErrorCode)
+			} else {
+				for _, device := range r.Data.WiredDevices {
+					deviceReport(model.Interface{IPv4Address: device.IPAddress})
+				}
+				for _, device := range r.Data.WirelessDevices {
+					deviceReport(model.Interface{IPv4Address: device.IPAddress})
+				}
+			}
+		}
+	}
 }
 
 func (t *tplinkTracker) Ping(model.Device) {
-
+	// Nothing to be done here. The tracker is purely asynchronous.
 }
